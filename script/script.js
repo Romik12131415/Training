33a1911b462733080f305df360b70d2aea3fb799
Roman Shevtsov <romik12131415@gmail.com>
2021-08-20 18:38:48 +0300

    First step in first application

--- script/script.js
+++ script/script.js
@@ -1,3 +1,21 @@
 "use strict";
-let a =undefined;
-console.log (typeof(null));
+let numberOfFilms = +prompt ("Сколько фильмов вы уже посмотрели?", "");
+
+let personalMovieDB = {
+    count: numberOfFilms,
+    movies: {},
+    actors: {},
+    genres: [],
+    private: false,
+};
+let a = prompt ("Один из последних просмотренных фильмов?",""),
+    b = prompt ("На сколько оцените его?",""),
+    c = prompt ("Один из последних просмотренных фильмов?",""),
+    d = prompt ("На сколько оцените его?","");
+
+personalMovieDB.movies[a] = b;
+personalMovieDB.movies[c] = d;
+
+
+console.log (numberOfFilms);
+console.log (personalMovieDB);
